bf1364073131285acc138d4a8b8cd27da5335c92
Rachel Wong <rwrachelwong@gmail.com>
2018-12-04 21:58:46 +0000

    Exercise 4: Loading a subset of stores

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,22 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+#4.1
+surrey = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+
+whistler = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+
+yaletown = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+#4.2
+@mens_stores = Store.where(mens_apparel: true).where(womens_apparel: false)
+
+#4.3
+@mens_stores.each { |store| puts store.name, store.annual_revenue }
+
+#4.4.
+Store.where(mens_apparel: false).where(womens_apparel: true).each do |store| 
+  if store.annual_revenue < 1000000 
+    puts store.name 
+  end
+end
